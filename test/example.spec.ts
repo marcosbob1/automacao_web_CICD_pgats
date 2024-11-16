b606bfa41567379c8aa04f7399b647c2583a3ff0
Marcos Antonio Pereira Neto <marcosbob1@hotmail.com>
2024-11-16 14:41:01 -0300

    alterando titulo da pag de playwright

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test('example test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   const title = await page.title();
-  expect(title).toBe('Fast and reliable end-to-end testing for modern web apps | Playwright');
+  expect(title).toContain('Playwright')
+  // alterando de: toBe('Fast and reliable end-to-end testing for modern web apps | Playwright');
 });
